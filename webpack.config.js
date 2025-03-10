dea045105051573fe3ff3e48a91da81f88d07a7d
ayugunin <ayugunin@sberautotech.ru>
2025-03-10 09:16:26 +0300

    fix: public url

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,65 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 
-const isProd = process.env.NODE_ENV === "production";
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  const publicPath = isProd ? "/airplane-logs/" : "/"
 
-const publicPath = isProd ? "/airplane-logs/" : "/"
-
-module.exports = {
-  mode: isProd ? 'production' : 'development',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.wasm$/,
-        type: 'javascript/auto',
-        loader: 'file-loader',
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|xml)$/,
-        loader: 'file-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "public", to: ".", globOptions: { ignore: ["**/index.html"] }  },
-        { from: 'node_modules/cesium/Build/Cesium', to: 'cesium' }
+  return {
+    mode: argv.mode,
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.wasm$/,
+          type: 'javascript/auto',
+          loader: 'file-loader',
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|svg|xml)$/,
+          loader: 'file-loader',
+        },
       ],
-    }),
-    new DefinePlugin({
-      CESIUM_BASE_URL: JSON.stringify(`${publicPath}cesium`),
-    }),
-  ],
-  devServer: {
-    static: path.join(__dirname, 'public'),
-    port: 3000,
-  },
-  experiments: {
-    asyncWebAssembly: true,
-    syncWebAssembly: true,
-  },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        publicPath
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: "public", to: ".", globOptions: { ignore: ["**/index.html"] }  },
+          { from: 'node_modules/cesium/Build/Cesium', to: 'cesium' }
+        ],
+      }),
+      new DefinePlugin({
+        CESIUM_BASE_URL: JSON.stringify(`${publicPath}cesium`),
+      }),
+    ],
+    devServer: {
+      static: path.join(__dirname, 'public'),
+      port: 3000,
+    },
+    experiments: {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+    },
+  }
 };
